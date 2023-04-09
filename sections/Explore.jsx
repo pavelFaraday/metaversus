c381b56c9aad297f9ef84ee711f7abc6da9b5064
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-04-09 16:44:50 +0400

    finish custom Text

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,33 @@
-'use client';
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { staggetContainer } from "../utils/motion";
+import { ExploreCard, TitleText, TypingText } from "../components";
+import { exploreWorlds } from "../constants";
 
 const Explore = () => (
-  <section>
-    Explore section
-  </section>
+	<section className={`${styles.paddings}`} id="explore">
+		<motion.div
+			variants={staggetContainer}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: false, amount: 0.25 }}
+			className={`${styles.innerWidth} mx-auto flex flex-col`}
+		>
+			<TypingText title="| The world" textStyles="text-center" />
+			<TitleText
+				title={
+					<>
+						Choose the world you want
+						<br className="md:block hidden" />
+						to explore
+					</>
+				}
+				textStyles="text-center"
+			/>
+		</motion.div>
+	</section>
 );
 
 export default Explore;
